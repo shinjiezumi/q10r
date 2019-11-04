4afc8f80d5cec5caa72aefaae28e293717b3a8ca
shinjiezumi <41136277+s9iwork@users.noreply.github.com>
2019-11-04 11:58:46 +0900

    認証API作成

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,4 +37,23 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+	/**
+	 * {@inheritdoc}
+	 */
+    protected function authenticated(Request $request, $user)
+	{
+		return $user;
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function loggedOut(Request $request)
+	{
+		// セッションを再生成する
+		$request->session()->regenerate();
+
+		return response()->json();
+	}
 }
--- app/Repositories/User.php
+++ app/Repositories/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Repositories;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
